87beca7ea2ec5dbd413bbfb0a3c5ec7feab756d5
mihailgaberov <mihail.gaberov@gmail.com>
2024-12-07 12:22:28 +0100

    :memo: Hoisting

--- Index.js
+++ Index.js
@@ -14,3 +14,16 @@ test();
 // Question: What will be logged after both calls to fn()?
 // 1
 // 2
+
+// 2. Hoisting
+console.log(a);
+var a = 5;
+function foo() {
+  console.log(a);
+  var a = 10;
+}
+foo();
+// Question: What will be logged and why?
+// undefined
+// undefined
+// because var hoists the declaration and initializes it to 'undefined'
